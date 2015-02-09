18edabf329d2e393fc00d5b6fb4855d4036aa047
Jeff Zheng <jeff.zheng.6@gmail.com>
2015-02-08 22:03:09 -0800

    refreshed index styling

--- festiv_content/static/js/myButton.js
+++
@@ -1,9 +0,0 @@
-$('#frontPageSearchIcon').click(function() { 
-    $( this ).css("opacity", 0);
-    $('#frontPageSearch').css("opacity", 1);
-}
-
-$('#frontPageSearch').click(function() { 
-    $( this ).css("opacity", 0);
-    $('#frontPageSearchIcon').css("opacity", 1);
-}
